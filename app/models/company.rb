6b59e09c238f918734a7be0e9c45a43927d494d1
gp <gpaul.rootuser@gmail.com>
2021-01-19 01:00:16 +0530

    Add company address

--- app/models/company.rb
+++ app/models/company.rb
@@ -1,5 +1,17 @@
+require 'memoist'
+
 class Company < ApplicationRecord
+  extend Memoist
+
   has_rich_text :description
 
   validates_with CompanyEmailValidator
+
+  def address
+    unless zip_code.empty? 
+      return ZipCodes.identify(zip_code)
+    end
+  end
+  memoize :address
+
 end
